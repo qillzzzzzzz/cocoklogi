df439e2e2703a932d91272e8292f22ceb54454f3
qillzzzzzzz <qilasolo20@gmail.com>
2026-01-14 11:42:27 +0700

    footer.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <p>
+        Made with <span>♡</span> for every love story
+    </p>
+    <small>
+        © <?php echo date("Y"); ?> Love Calculator
+    </small>
+</footer>
